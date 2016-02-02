6d718eeff91df7acdcfb3c58a84cf570ad2a4538
Emmitt luna <emmittluna@Emmitts-MacBook-Air.local>
2016-02-02 15:43:42 -0600

    PLProject1 (created classes)

---
+++ src/Luna/customer/CustomerApp.java
@@ -0,0 +1,9 @@
+package Luna.customer;
+
+import java.util.Scanner;
+
+/**
+ * Created by emmittluna on 2/2/16.
+ */
+public class CustomerApp {
+}
---
+++ src/Luna/customer/CustomerDB.java
@@ -0,0 +1,7 @@
+package Luna.customer;
+
+/**
+ * Created by emmittluna on 2/2/16.
+ */
+public class CustomerDB {
+}
