658e0828bbb1553dcd1f9499502e23b1ac180bf7
Emmitt luna <emmittluna@wireless-10-147-163-94.public.utexas.edu>
2016-02-02 20:19:08 -0600

    PLProject1(Feb.2.2016 updates)

--- src/Luna/customer/Customer.java
+++ src/Luna/customer/Customer.java
@@ -1,6 +1,32 @@
 package Luna.customer;
 
 public class Customer {
+    private String name;
+    public void setName(String person){name = person;}
+    public String getName(){return name;}
+
+    private String address;
+    public void setAddress(String place){address = place;}
+    public String getAddress(){return address;}
+
+    private String city;
+    public void setCity(String town){city = town;}
+    public String getCity(){return city;}
+
+    private String state;
+    public void setState(String st){state = st;}
+    public String getState(){return state;}
+
+    private String postalCode;
+    public void setPostalCode(String zip){postalCode = zip;}
+    public String getPostalCode(){return postalCode;}
+
+
+    public String getNameAndAddress () {
+        return getName() + '\n' + getAddress() + '\n' +
+                getCity() + ',' + getState() + getPostalCode() ;
+    }
+
 
 
 }
--- src/Luna/customer/CustomerApp.java
+++ src/Luna/customer/CustomerApp.java
@@ -6,4 +6,15 @@ import java.util.Scanner;
  * Created by emmittluna on 2/2/16.
  */
 public class CustomerApp {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Dude\n");
+        System.out.println("Enter Your number: ");
+        Scanner sc = new Scanner(System.in);
+        int customerNumber = sc.nextInt();
+        System.out.println("Your ID is: " + customerNumber);
+
+
+    }
+
 }
