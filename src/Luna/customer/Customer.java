79fb70150df37555b715e429e5fd8ac1ccbf97fa
Emmitt luna <emmittluna@wireless-10-147-163-94.public.utexas.edu>
2016-02-02 18:51:01 -0600

    PLProject1

--- src/Luna/customer/Customer.java
+++ src/Luna/customer/Customer.java
@@ -2,9 +2,5 @@ package Luna.customer;
 
 public class Customer {
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
 
 }
--- src/Luna/customer/CustomerDB.java
+++ src/Luna/customer/CustomerDB.java
@@ -1,7 +1,4 @@
 package Luna.customer;
 
-/**
- * Created by emmittluna on 2/2/16.
- */
 public class CustomerDB {
 }
